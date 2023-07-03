e4098c8dda19ea73d7bf2ee372d9a3c00509a6a3
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-07-03 09:41:49 +0300

    Created buttons, added useState, changed style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div >
-
-    </div>
-  );
+    const [cash, setCash] = useState<number>(0)
+    const addCash = (value: number) => {
+        setCash(value)
+    }
+    const getCash = (value: number) => {
+        setCash(value)
+    }
+    return (
+        <div>
+            <div>{cash}</div>
+            <div>
+                <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
+                <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
